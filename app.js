10ac6a5455ee2991759e8e948b0c2200ba0ccdd8
Rupesh Chand <rupeshchandvasantam@gmail.com>
2024-11-23 14:27:22 +0530

    main page completed

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
-let latestProducts = document.querySelector(".row");
+let latestProducts = document.querySelector(".lp-row");
 console.log(latestProducts);
 
 fetch("https://fakestoreapi.com/products").then(res => res.json()).then((data)=>{
     data.forEach((ele,i)=>{
         let cards = `
-        <div class="col">
             <div class="card">
-                <img src="${ele.image}" class="card-img-top" alt="images">
                 <div class="card-body">
+                <img src="${ele.image}" class="card-img-top" alt="images">
                     <h5 class="card-title">${ele.title}</h5>
                     <p class="card-text">${ele.description}</p>
-                    <p class="card-price">${ele.price}</p>
-                    <div class="lp-buttons">
+                    <hr>
+                    <p class="card-price">$ ${ele.price}</p>
+                    <hr>
+                    <div class="card-buttons">
                         <a href="" class="btnOne"><button>Details</button></a>
-                        <a href="" class="btnTwo"><button>Add to cart</button></a>
+                        <a href="cart.html" class="btnTwo"><button>Add to cart</button></a>
                     </div>
                 </div>
             </div>
-        </div>
         `
         latestProducts.insertAdjacentHTML("beforeend",cards)
+    })
+
+    document.querySelectorAll(".card-title").forEach((title)=>{
+        const maxLength = 11;
+        if(title.textContent.length > maxLength){
+            title.textContent = title.textContent.slice(0,maxLength)+ "...";
+        }
+    })
+    document.querySelectorAll(".card-text").forEach((text)=>{
+        const maxLength = 90;
+        if(text.textContent.length > maxLength){
+            text.textContent =  text.textContent.slice(0,maxLength)+"...";
+        }
     })
 })
 .catch((err)=>{
     console.log(err)
 })
+
+
