f1d73b25d80010e8df9f0353cbeed9d0fe38ad9b
Rupesh Chand <rupeshchandvasantam@gmail.com>
2024-11-27 23:55:07 +0530

    filter products is completed

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+// fetching data into home page
 let latestProducts = document.querySelector(".lp-row");
 console.log(latestProducts);
+let fetchedData = [];
 
 fetch("https://fakestoreapi.com/products").then(res => res.json()).then((data)=>{
+    fetchedData = data;  
+    displayProducts(fetchedData)  
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+function displayProducts(data){
+    latestProducts.innerHTML = "";
     data.forEach((ele,i)=>{
         let cards = `
             <div class="card">
@@ -20,23 +31,47 @@ fetch("https://fakestoreapi.com/products").then(res => res.json()).then((data)=>
             </div>
         `
         latestProducts.insertAdjacentHTML("beforeend",cards)
+        document.querySelectorAll(".card-title").forEach((title)=>{
+            const maxLength = 11;
+            if(title.textContent.length > maxLength){
+                title.textContent = title.textContent.slice(0,maxLength)+ "...";
+            }
+        })
+        document.querySelectorAll(".card-text").forEach((text)=>{
+            const maxLength = 90;
+            if(text.textContent.length > maxLength){
+                text.textContent =  text.textContent.slice(0,maxLength)+"...";
+            }
+        })
     })
+}
 
-    document.querySelectorAll(".card-title").forEach((title)=>{
-        const maxLength = 11;
-        if(title.textContent.length > maxLength){
-            title.textContent = title.textContent.slice(0,maxLength)+ "...";
-        }
-    })
-    document.querySelectorAll(".card-text").forEach((text)=>{
-        const maxLength = 90;
-        if(text.textContent.length > maxLength){
-            text.textContent =  text.textContent.slice(0,maxLength)+"...";
-        }
-    })
+
+//filtering the data based on category wise
+const all = document.querySelector("#all-button")
+const menCloth = document.querySelector("#men-button")
+const womenCloth = document.querySelector("#women-button")
+const jewelery = document.querySelector("#jewel-button")
+const electronics = document.querySelector("#ele-button")
+
+all.addEventListener("click",()=>{
+    displayProducts(fetchedData)
 })
-.catch((err)=>{
-    console.log(err)
+menCloth.addEventListener("click",()=>{
+    let filterMen = fetchedData.filter((ele)=>{
+        return ele.category == "men's clothing"
+    })
+    displayProducts(filterMen)
 })
-
-
+womenCloth.addEventListener("click",()=>{
+    let filterWomen = fetchedData.filter((ele)=> ele.category == "women's clothing")
+    displayProducts(filterWomen)
+})
+jewelery.addEventListener("click",()=>{
+    let filterJewelery = fetchedData.filter((ele)=> ele.category == "jewelery")
+    displayProducts(filterJewelery)
+})
+electronics.addEventListener("click",()=>{
+    const filterElectronics = fetchedData.filter((ele)=> ele.category == "electronics")
+    displayProducts(filterElectronics)
+})
