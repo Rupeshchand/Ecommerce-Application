7111b4492ef5ed7b6a41a565262296ca99fcee59
Rupesh Chand <rupeshchandvasantam@gmail.com>
2024-12-03 23:08:31 +0530

    product details functionality has been added

--- app.js
+++ app.js
@@ -1,182 +1,169 @@
 //filtering the data based on category wise
-let latestProducts =  document.querySelector(".row")
-let all = document.querySelector("#allButton")
-let menCloth = document.querySelector("#men-button")
-let womenCloth = document.querySelector("#women-button")
-let jewelery = document.querySelector("#jewel-button")
-let electronics = document.querySelector("#ele-button")
-let container =  document.querySelector("#cartContainer")
+const latestProducts =  document.querySelector(".row")
+const all = document.querySelector("#allButton")
+const menCloth = document.querySelector("#men-button")
+const womenCloth = document.querySelector("#women-button")
+const jewelery = document.querySelector("#jewel-button")
+const electronics = document.querySelector("#ele-button")
+const container =  document.querySelector("#cartContainer")
+const url =  new URLSearchParams(window.location.search)
+const productId =  url.get('id')
+
 let fetchedData = [];
-
-
-// fetching data into home page
-fetch("https://fakestoreapi.com/products").then(res => res.json()).then((data)=>{
-    fetchedData = data;  
-    displayProducts(fetchedData)  
-})
-.catch((err)=>{
-    console.log(err);
-})
+let cart = []
 
 //fun to display products
-function displayProducts(data){
-    latestProducts.innerHTML = "";
-    data.forEach((product)=>{
-        let cards = `
-        <div class="col-lg-4 col-md-6">
-            <div class="card">
-                <img src="${product.image}" class="card-img-top" alt="images">
-                <div class="card-body">
-                    <h5 class="card-title">${product.title}</h5>
-                    <p class="card-text">${product.description}</p>
-                    <hr class="price-hr">
-                    <p class="card-price">$ ${product.price}</p>
-                    <hr class="price-hr">
-                    <div class="card-buttons">
-                        <a href=""><button class="btnOne">Details</button></a>
-                        <a href="cart.html"><button class="btnTwo">Add to cart</button></a>
+        const rltdProducts =  document.querySelector(".uml-productcontainer ")
+        
+        async function fetchData() {
+            try {
+                const response = await fetch('https://fakestoreapi.com/products');
+                const data = await response.json();
+                fetchedData = data
+                console.log(fetchedData);
+                
+                displayProducts(fetchedData)
+                slideProducts(fetchedData)
+                productDetails(fetchedData)
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+        function displayProducts(data){
+            latestProducts.innerHTML = "";
+            data.forEach((product)=> {
+                const truncateTitle =  truncateText(product.title,11)
+                const truncateDes = truncateText(product.description,90)
+                let cards = `
+                <div class="col-lg-4 col-md-6">
+                    <div class="card">
+                        <img src="${product.image}" class="card-img-top" alt="${product.title}">
+                        <div class="card-body">
+                            <h5 class="card-title">${truncateTitle}</h5>
+                            <p class="card-text">${truncateDes}</p>
+                            <hr class="price-hr">
+                            <p class="card-price">$ ${product.price}</p>
+                            <hr class="price-hr">
+                            <div class="card-buttons">
+                                <button class="btnOne" data-id="${product.id}" onclick="window.location.href='product-details.html?id=${product.id}'">Details</button>
+                                <button class="btnTwo" data-id="${product.id}">Add to cart</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        `
-        latestProducts.insertAdjacentHTML("beforeend",cards)
-        document.querySelectorAll(".card-title").forEach((title)=>{
-            const maxLength = 11;
-            if(title.textContent.length > maxLength){
-                title.textContent = title.textContent.slice(0,maxLength)+ "...";
+                `
+                latestProducts.insertAdjacentHTML("beforeend",cards)
+            })
+        }
+        function slideProducts(data){
+            if(rltdProducts){
+                data.forEach((product)=>{
+                    let productSlider = `
+                    <div class="prod">
+                        <img src="${product.image}" class="sliderImg" alt="${product.title}">
+                        <h3>${product.title}</h3>
+                        <div class="card-buttons">
+                            <button class="btnOne" data-id="${product.id}">Details</button>
+                            <button class="btnTwo" data-id="${product.id}">Add to cart</button>
+                        </div>
+                    </div>`
+                    rltdProducts.insertAdjacentHTML("beforeend",productSlider);
+                })
             }
-        })
-        document.querySelectorAll(".card-text").forEach((text)=>{
-            const maxLength = 90;
-            if(text.textContent.length > maxLength){
-                text.textContent =  text.textContent.slice(0,maxLength)+"...";
+        }
+        function filterProducts(category){
+            let filteredData = []
+            if(category == "all"){
+                filteredData = fetchedData;
             }
-        })
-    })
-}
-
-if(all){
-    all.addEventListener("click",()=>{
-        displayProducts(fetchedData)
-    })
-}
-if(menCloth){
-    menCloth.addEventListener("click",()=>{
-        let filterMen = fetchedData.filter((ele)=>{
-            return ele.category == "men's clothing"
-        })
-        displayProducts(filterMen)
-    })    
-}
-if(womenCloth){
-    womenCloth.addEventListener("click",()=>{
-        let filterWomen = fetchedData.filter((ele)=> ele.category == "women's clothing")
-        displayProducts(filterWomen)
-    })
-}
-if(jewelery){
-    jewelery.addEventListener("click",()=>{
-        let filterJewelery = fetchedData.filter((ele)=> ele.category == "jewelery")
-        displayProducts(filterJewelery)
-    })
-}
-if(electronics){
-    electronics.addEventListener("click",()=>{
-        let filterElectronics = fetchedData.filter((ele)=> ele.category == "electronics")
-        displayProducts(filterElectronics)
-    })    
-}
-
-//adding to cart
-let head = document.querySelector("#cardTitle");
-document.querySelectorAll(".btnTwo").forEach((btn,i)=>{
-    btn.addEventListener("click",()=>{
-        const product = fetchedData[i];
-        addToCart(product);
-        updateCartDisplay();
-    })
+            else{
+                filteredData =  fetchedData.filter((product)=> product.category == category)
+            }
+            displayProducts(filteredData)
+        }
+        
+        function truncateText(text,maxLength){
+            return text.length > maxLength ? text.slice(0,maxLength)+ "...":text
+        }
+        function productDetails(product){
+            let pdContainer = document.querySelector(".product-details");
+            if(pdContainer){
+                pdContainer.innerHTML = 
+                `
+                <div class="pd-part1">
+                    <img src="${product.image}" class="pd-img" alt="${product.title}">
+                </div>
+                <div class="pd-part2">
+                    <h5 class="pd-category">MEN'S CLOTHING</h5>
+                    <h2 class="pd-title">${product.title}</h2>
+                    <p class="pd-rating">${product.rating["rate"]}<sppan class="star">★</span></p>
+                    <p class="pd-price">$${product.price}</p>
+                    <p class="pd-desc">${product.description}</p>
+                    <div class="pd-btns">
+                        <button class="pd-btnTwo">Add to cart</button>
+                        <a href="cart.html"><button class="go-cart">Go to cart</button></a>
+                    </div>
+                </div>`
+            }
+        }
+        latestProducts.addEventListener("click",(e)=>{
+            const target = e.target
+            if(target.classList.contains("btnOne")){
+                const productId = target.dataset.id;
+                const product =  fetchedData.find((item)=>item.id == productId)
+                if(product){
+                    window.location.href=`product-details.html?id=${productId}`
+                }
+                else{
+                    console.error("Product not found")
+                }
+            }
+        
+            if(target.classList.contains("btnTwo")){
+                const productId = target.dataset.id
+                console.log("Add to Cart clicked. Product ID:", productId);
+        
+                const product = fetchedData.find((item) => item.id == productId)
+                if(product){
+                    addToCart(product)
+                }
+            }
+        })   
+        function addToCart(product){
+            const existingProduct = cart.find((item)=>item.id == product.id)
+            if(existingProduct){
+                existingProduct.quantity += 1;
+            }
+            else{
+                cart.push({...product,quantity:1})
+            }
+            updateCartCount();
+            saveCartToLocalStorage();
+        }
+        
+        function updateCartCount(){
+            const cartCount = document.querySelector(".cart-count")
+            if(cartCount){
+                cartCount.textContent =  cart.length
+            }
+        }
+        
+        function saveCartToLocalStorage() {
+            localStorage.setItem("cart",JSON.stringify(cart));
+        }
+        
+        function loadCartFromStorage(){
+            const savedCart = localStorage.getItem("cart")
+            if(savedCart){
+                cart =  JSON.parse(savedCart)
+                updateCartCount()
+            }
+        }
+        all?.addEventListener("click",()=> filterProducts("all"))
+        menCloth?.addEventListener("click",()=> filterProducts("men's clothing"))
+        womenCloth?.addEventListener("click",()=> filterProducts("women's clothing"))
+        jewelery?.addEventListener("click",()=> filterProducts("jewelery"))
+        electronics?.addEventListener("click",()=> filterProducts("electronics"))    
+document.addEventListener("DOMContentLoaded",()=>{
+    fetchData();
 })
-let cart = []
-function addToCart(product){
-    cart.push(product)    
-}
-function updateCartDisplay(){
-    let cartCon = document.querySelector("#cartContainer")
-    cartCon.innerHTML = "";
-    head.innerHTML = "";
-    if(cart.length == 0){
-        cartCon,innerHTML = "<p>Your cart is empty</p>";
-        return;
-    }
-    let cart = `
-    <table>
-        <thead>
-            <tr>
-                <th>Item<th>
-                <th>Quantity<th>
-                <th>Price<th>
-                <th>Total<th>
-            </tr>
-        </thead>
-    <tbody>
-}`
-let totalAmt = 0;
-cart.forEach(item=>{
-    const itemTotal = item.price*item.quantity;
-    totalAmt += itemTotal;
-    cart += `
-    <tr>
-        <td>${item.name}</td>
-        <td>
-            <button onclick = "decreaseQty("${item.name}")">-</button>
-            ${item.quantity}
-            <button onclick = "increaseQty("${item.name}")">+</button>
-        </td>
-        <td>$${item.price}</td>
-        <td>$${itemTotal}</td>
-    </tr>
-`
-})
-
-cart += `</tbody>
-    </table>
-    <div>
-        <p>Total Amount: $${totalAmt}</p>
-        <button onclick="checkout()">Checkout</button>
-    </div>`
-
-    cartCon.innerHTML = cart;
-}
-
-function increaseQty(productName){
-    const product = cart.find(item=>item.name == productName);
-    if(product){
-        product.quantity++;
-        updateCartDisplay();
-    }
-}
-
-function decreaseQty(productName){
-    const product = cart.find(item=>item.name == productName);
-    if(product && product.quantity>1){
-        product.quantity--;
-    }
-    else{
-        cart = cart.filter(item=>item.name != productName)
-    }
-    updateCartDisplay()
-}
-
-function saveCartToStorage(){
-    localStorage.setItem("cart", JSON.stringify(cart));
-}
-function loadCartFromStorage() {
-    const savedCart = localStorage.getItem("cart");
-    if(savedCart){
-        cart = JSON.parse(savedCart);
-        updateCartDisplay()
-    }    
-}
-
-window.onload = loadCartFromStorage
